92c56f447c3f213636e90e0dd56f857bc0113a38
Jacek Bilski <jacek.bilski@innoq.com>
2018-04-25 14:19:24 +0200

    Tests not using Spring at all.

--- src/test/java/com/example/demo/FooTest.java
+++ src/test/java/com/example/demo/FooTest.java
@@ -1,16 +1,11 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@ContextConfiguration(classes = DemoApplication.class)
-@ExtendWith(SpringExtension.class)
 class FooTest {
     private Bar bar = mock(Bar.class);
     private Foo foo = new Foo(bar);
